75e6abd169f688d9984df80cb5dac2d6e67ef4b7
j-plou <jesusplou@gmail.com>
2023-09-28 11:41:58 +0200

    Update matrix iteration with map method

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -11,23 +11,31 @@ export class Matrix {
   }
 
   private processRows(source_rows: string[]) {
+    /*
     for (let row of source_rows) {
       let new_row: number[] = [];
       for (let cell of row.split(" ")) {
         new_row.push(Number(cell));
       }
       this._rows.push(new_row);
-    }
+    } */
+    this._rows = source_rows.map((row) =>
+      row.split(" ").map((cell) => Number(cell))
+    );
   }
 
   private processColumns(total_cols: number) {
+    /*
     for (let i = 0; i < total_cols; i++) {
       let new_column: number[] = [];
       for (let row of this._rows) {
         new_column.push(row[i]);
       }
       this._columns.push(new_column);
-    }
+    } */
+    this._columns = Array.from({ length: total_cols }, (_, i) =>
+      this._rows.map((row) => row[i])
+    );
   }
 
   get rows(): number[][] {
