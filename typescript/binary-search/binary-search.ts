f471bc3d5ca09b57b163d81a13680316436bc0b2
j-plou <jesusplou@gmail.com>
2023-11-09 15:28:20 +0100

    Refactor find method

--- typescript/binary-search/binary-search.ts
+++ typescript/binary-search/binary-search.ts
@@ -1,23 +1,18 @@
-export function find(haystack: number[], needle: number): number | never {
+export function find(haystack: number[], needle: number): number {
   let middleIndex: number = Math.floor(haystack.length / 2);
-  let splitHaystack: number[] = haystack
+  let splittedHaystack: number[] = haystack
 
-  while (needle != splitHaystack[middleIndex]) {
-    if (splitHaystack.length <= 1) {
+  while (needle != splittedHaystack[middleIndex]) {
+    if (splittedHaystack.length <= 1) {
         throw new Error('Value not in array')
     }
-    if (needle > splitHaystack[middleIndex]) {
-        if (middleIndex == splitHaystack.length + 1) {
-            splitHaystack = splitHaystack.slice(middleIndex);
-        } else {
-            splitHaystack = splitHaystack.slice(middleIndex + 1);            
-        }
-    }
-    if (needle < splitHaystack[middleIndex]) {
-        splitHaystack = splitHaystack.slice(0, middleIndex);
-    }
 
-    middleIndex = Math.floor(splitHaystack.length / 2);
+    if (needle > splittedHaystack[middleIndex]) {
+        splittedHaystack = splittedHaystack.slice(middleIndex);
+    } else {
+        splittedHaystack = splittedHaystack.slice(0, middleIndex);
+    }
+    middleIndex = Math.floor(splittedHaystack.length / 2);
   }
 
   return haystack.findIndex((element) => element == needle);
